df10b5c6d6b5d8ad44aabef4f2388ef067895d31
柴田成clo <shibata.naru@cloco.co.jp>
2023-12-25 17:52:35 +0900

    commit

--- my-weather-app/src/pages/TopPage/index.vue
+++ my-weather-app/src/pages/TopPage/index.vue
@@ -1,17 +1,27 @@
 <template>
     <q-layout>
-        <div class="">
-            <div class="row">
-                <div class=" bg-blue-1 justify-between q-mr-lg" style="width:80%">
-                    <div class="q-pa-xl">
-                        <h2 class="text-primary text-weight-regular text-bold">weather</h2>
-                        <div class="q-mt-md text-h5">{{ today }}</div>
+        <div>
+            <div class="row justify-center no-wrap ">
+                <div class="  justify-between q-mr-lg  body--lithe" :class="{'body--dark': mode}">
+                    <div class="q-pa-xl row justify-between">
+                        <div>
+                        <h2 class="text-blue-6  text-weight-bold">weather</h2>
+                        <div class="q-mt-md text-indigo-8 text-h5 text-weight-bold">{{ today }}</div>
+                        </div>
+                        <q-btn
+                            class="hoge"
+                            color="white"
+                            text-color="black"
+                            label="Toggle"
+                            @click="toggleDark()"
+                        />
                     </div>
                     <div class="row justify-around">
                     <q-card v-for="weather in weatherSituation"
                             @click="selectCard(weather)"
                             :weather="weather"
-                            bordered class="my-card q-p-md q-ma-lg cursor-pointer situation-card"
+                            bordered class="my-card q-p-md q-ma-lg cursor-pointer situation-card "
+                            :class="{'card--dark' : mode}"
                             style="width: 15%"
                     >
                         <q-card-section class="q-pt-none">
@@ -26,10 +36,10 @@
                         </q-card-section>
 
                         <q-separator inset/>
-                        <q-card-section class="text-h6 text-center text-grey-7">
+                        <q-card-section class="text-h6 text-center text-grey-7 " :class="{'text-white' : mode}">
                             {{ weather.dt_txt.split(' ')[0] }}
                         </q-card-section>
-                        <q-card-section class="text-center text-bold text-h5 text-grey-7">
+                        <q-card-section class="text-center text-bold text-h5 text-grey-7" :class="{'section-text--white' : mode}">
                             {{ Math.round(weather.main.temp) }}°
                         </q-card-section>
                     </q-card>
@@ -42,8 +52,8 @@
                         <hum-monitor :selected-card="selectedWeather" :detail-weather-data="timeObjects"></hum-monitor>
                     </div>
                 </div>
-                <div>
-                        <select-day class="scroll__card" style="width: 330px;" :selected-card="selectedWeather"></select-day>
+                <div class="bg-white" :class="{'section--dark' : mode}" style="width: 20%">
+                        <select-day class="scroll__card " style="width: 100%; margin-left: 5%"   :selected-card="selectedWeather"></select-day>
                 </div>
             </div>
 
@@ -58,6 +68,7 @@ import TempMonitor from "@/components/TempMonitor.vue";
 import fetchWeather from "@/api/api.ts"
 import {ref, computed, onMounted} from "vue";
 import gsap from "gsap";
+import { useQuasar } from 'quasar'
 import WeatherForecast from "@/components/WeatherForecast.vue";
 const today = new Date().toLocaleDateString('ja-JP');
 const response = await fetchWeather();
@@ -84,6 +95,13 @@ const selectedWeather = ref(weatherSituation[0])
 function selectCard(weather) {
     selectedWeather.value = weather;
 }
+
+const mode = ref(false);
+
+
+const toggleDark = () => {
+    mode.value = !mode.value;
+}
 </script>
 
 <style>
@@ -101,4 +119,26 @@ html {
     position: sticky;
     top: 0;
 }
+.body--lithe{
+    color: white;
+    background-color: #d1eafd;
+}
+
+.body--dark {
+    color: white;
+    background-color: #223b93;
+}
+
+.section--dark{
+    background-color: #0e2269;
+}
+.section-text--white{
+    color: white;
+}
+
+.card--dark{
+    color: white;
+    background-color: rgb(54,70,128);
+}
+
 </style>
